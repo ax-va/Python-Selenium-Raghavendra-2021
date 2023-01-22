3b75100f0e4727d1c89e38723e90eb94792a6752
Alex Vasiliev <vasiliev.informatik@t-online.de>
2023-01-22 14:55:23 +0100

    update

---
+++ examples/example-1-introduction.py
@@ -0,0 +1,24 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Open web Chrome browser using webdriver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+# Add URL to open in browser
+driver.get("http://www.google.com")
+# Find Element of button "Reject all"
+button_reject_all = driver.find_element(By.XPATH, "(//button)[4]")
+# Click button
+button_reject_all.click()
+# Find Element of search bar
+search = driver.find_element(By.NAME, "q")
+# Write text
+search.send_keys("github ax-va")
+# Submit text to search bar
+search.submit()
+# Wait 10 seconds
+time.sleep(10)
+# Close Firefox browser
+driver.quit()
