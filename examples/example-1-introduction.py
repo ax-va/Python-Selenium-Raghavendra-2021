6b85b02e8879f8dc5b8f5a1d420105f8e558acf9
Alex Vasiliev <vasiliev.informatik@t-online.de>
2023-01-22 18:31:48 +0100

    update

--- examples/example-1-introduction.py
+++ examples/example-1-introduction.py
@@ -9,11 +9,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 # Add URL to open in browser
 driver.get("http://www.google.com")
-# Find Element of button "Reject all"
+# Find element of button "Reject all"
 button_reject_all = driver.find_element(By.XPATH, "(//button)[4]")
 # Click button
 button_reject_all.click()
-# Get Element of search bar
+# Get element of search bar
 search = driver.find_element(By.NAME, "q")
 # Write text to search
 search.send_keys("github ax-va")
