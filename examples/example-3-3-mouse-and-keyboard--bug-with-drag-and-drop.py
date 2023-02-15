be14a0fd75ed6b094fab5224b03999f792e92cdc
ax-va <vasiliev.informatik@t-online.de>
2023-02-15 19:18:36 +0100

    update

--- examples/example-3-3-mouse-and-keyboard--bug-with-drag-and-drop.py
+++
@@ -1,9 +0,0 @@
-# Drag-and-drop of Selenium does not work:
-"""
-element = driver.find_element(By.ID, "source")
-target = driver.find_element(By.ID, "target")
-
-from selenium.webdriver import ActionChains
-actions = ActionChains(driver)
-actions.drag_and_drop(element, target).perform()
-"""
---
+++ examples/example-3-3-mouse-and-keyboard--drag-and-drop-[BUG].py
@@ -0,0 +1,38 @@
+"""
+Selenium has bugs with drag-and-drop:
+1) Coordinates are determined not correctly;
+2) 'Release' does not work.
+"""
+import os
+import pathlib
+import sys
+import time
+
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+
+# Get the package directory
+package_dir = str(pathlib.Path(__file__).resolve().parents[1])
+# Add the package directory into sys.path if necessary
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
+
+# my modules
+import utils.webdrivers as webdrivers
+
+
+driver = webdrivers.get_chromedriver()
+website_abspath = os.path.abspath("../websites/drag-and-drop/drag-and-drop.html")
+driver.get("file:///" + website_abspath)
+time.sleep(5)
+
+source1 = driver.find_element(By.ID, "source1")
+target1 = driver.find_element(By.ID, "target1")
+
+actions = ActionChains(driver)
+# actions.drag_and_drop(source1, target1).pause(5).perform()
+# actions.click_and_hold(source1).move_to_element(target1).pause(5).release().pause(5).perform()
+actions.click_and_hold(source1).move_by_offset(120, 55).pause(5).release().pause(5).perform()
+
+# Closes all the open windows and terminate the process for the driver
+driver.quit()
