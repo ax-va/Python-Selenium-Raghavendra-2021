a8653f9f0a89b27aaddc3efb0f9f9dd8f6e2f275
ax-va <vasiliev.informatik@t-online.de>
2023-02-15 19:52:25 +0100

    update

--- examples/example-3-3-mouse-and-keyboard--drag-and-drop-BUGS.py
+++ examples/example-3-3-mouse-and-keyboard--drag-and-drop-BUGS.py
@@ -1,5 +1,5 @@
 """
-Selenium has bugs with drag-and-drop and actions:
+Selenium has bugs with drag-and-drop and action chains:
 1) It does not work (Chrome, Firefox, Windows);
 2) Positions are determined not correctly (Chrome, Firefox, Ubuntu);
 3) release() does not work (Chrome, Firefox, Ubuntu).
