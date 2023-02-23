545b0399324bc083fb5987e3b3e5b1d4d72d3f58
ax-va <vasiliev.informatik@t-online.de>
2023-02-23 14:33:59 +0100

    update

---
+++ examples/example-4-6-web-elements--partial-link-locator.py
@@ -0,0 +1,34 @@
+import os
+import pathlib
+import sys
+import time
+
+from selenium.webdriver.common.by import By
+
+# Get the package directory
+package_dir = str(pathlib.Path(__file__).resolve().parents[1])
+# Add the package directory into sys.path if necessary
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
+
+# my modules
+import utils.webdrivers as webdrivers
+from utils.highlighter import Highlighter
+
+
+driver = webdrivers.get_chromedriver()
+highlighter = Highlighter(driver, wait_in_sec=2)
+website_abspath = os.path.abspath("../webpages/categories/index.html")
+driver.get("file:///" + website_abspath)
+time.sleep(5)
+
+# Locate elements using partial link text
+anchor1 = driver.find_element(By.PARTIAL_LINK_TEXT, "Py")
+highlighter.highlight_and_wait(anchor1)
+anchor2 = driver.find_element(By.PARTIAL_LINK_TEXT, "taba")
+highlighter.highlight_and_wait(anchor2)
+
+# anchor3 = driver.find_element(By.LINK_TEXT, "py")
+# selenium.common.exceptions.NoSuchElementException: Message: no such element: Unable to locate element: ...
+
+driver.quit()
