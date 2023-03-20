575efbff9c66823999ba7b8ab01fb169ecbfdbf0
ax-va <vasiliev.informatik@t-online.de>
2023-03-20 17:04:11 +0100

    update

---
+++ examples/example-7-2-iframes-and-textboxes--textboxes.py
@@ -0,0 +1,53 @@
+import os
+import pathlib
+import sys
+import time
+
+from selenium.webdriver.common.by import By
+
+# Get the package directory
+package_dir = str(pathlib.Path(__file__).resolve().parents[1])
+# Add the package directory into sys.path if necessary
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
+
+# my modules
+import utils.webdrivers as webdrivers
+
+
+HTML_CODE = """
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Textboxes</title>
+    </head>
+    <body>
+        <h1>Single Line Textbox</h1>
+        <label for="book1">Book Name:</label>
+        <input type="text" id="book1" name="book">
+        <h1>Textarea</h1>
+        <label for="book2">Book Name:</label>
+        <br/>
+        <textarea id="book2" cols="50" rows="5"></textarea>
+    </body>
+</html>
+"""
+
+with open("../webpages/textboxes/index.html", "w") as f:
+    f.write(HTML_CODE)
+
+driver = webdrivers.get_chromedriver()
+website_abspath = os.path.abspath("../webpages/textboxes/index.html")
+driver.get("file:///" + website_abspath)
+time.sleep(5)
+
+input_book_name = driver.find_element(By.ID, "book1")
+input_book_name.send_keys("Selenium")
+time.sleep(5)
+
+textarea_book_name = driver.find_element(By.ID, "book2")
+textarea_book_name.send_keys("Selenium Selenium \n Selenium")
+time.sleep(5)
+
+driver.quit()
