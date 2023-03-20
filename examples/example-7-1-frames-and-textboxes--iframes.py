b41295f6c24d26cfdf979b6c1ea4c3db06a99f81
ax-va <vasiliev.informatik@t-online.de>
2023-03-20 16:47:05 +0100

    update

--- examples/example-7-1-frames-and-textboxes--iframes.py
+++ examples/example-7-1-frames-and-textboxes--iframes.py
@@ -4,6 +4,8 @@ import sys
 import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as e_cs
+from selenium.webdriver.support.wait import WebDriverWait
 
 # Get the package directory
 package_dir = str(pathlib.Path(__file__).resolve().parents[1])
@@ -25,11 +27,11 @@ HTML_CODE = """
     <body>
         <div>
             <h5>IFrame0</h5>
-            <iframes id="iframe0" name="apress" src="https://www.apress.com" height="600" width="600"></iframes>
+            <iframe id="iframe0" name="apress" src="https://www.apress.com" height="600" width="600"></iframe>
         </div>
         <div>
             <h5>IFrame1</h5>
-            <iframes id="iframe1" name="wiki" src="https://www.wikipedia.org" height="600" width="600"></iframes>
+            <iframe id="iframe1" name="wiki" src="https://www.wikipedia.org" height="600" width="600"></iframe>
         </div>
     </body>
 </html>
@@ -79,13 +81,33 @@ driver.switch_to.frame(1)
 driver.switch_to.parent_frame()
 
 # Switch to frame with WebElement
-driver.switch_to.frame(driver.find_element(By.XPATH, "//iframes[@src='https://www.apress.com']"))
+driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe[@src='https://www.apress.com']"))
 # Switch to the parent frame
 driver.switch_to.parent_frame()
 
 # Switch to frame with WebElement
-driver.switch_to.frame(driver.find_element(By.XPATH, "//iframes[@src='https://www.wikipedia.org']"))
+driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe[@src='https://www.wikipedia.org']"))
 # Switch to the parent frame
 driver.switch_to.parent_frame()
+
+driver.get("file:///" + website_abspath)
+WebDriverWait(driver, 5).until(e_cs.frame_to_be_available_and_switch_to_it("apress"))
+
+driver.get("file:///" + website_abspath)
+WebDriverWait(driver, 5).until(e_cs.frame_to_be_available_and_switch_to_it("iframe0"))
+
+driver.get("file:///" + website_abspath)
+WebDriverWait(driver, 5).until(
+    e_cs.frame_to_be_available_and_switch_to_it(
+        driver.find_element(By.XPATH, "//iframe[@src='https://www.apress.com']")
+    )
+)
+
+driver.get("file:///" + website_abspath)
+WebDriverWait(driver, 5).until(
+    e_cs.frame_to_be_available_and_switch_to_it(
+        driver.find_element(By.CSS_SELECTOR, "iframe:nth-of-type(1)")
+    )
+)
 
 driver.quit()
