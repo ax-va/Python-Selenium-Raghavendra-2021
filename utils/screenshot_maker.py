7c8e22412d02d71b147eceb5b7b5c0c0afd431f1
ax-va <vasiliev.informatik@t-online.de>
2023-03-30 22:31:52 +0200

    update

--- utils/screenshot_maker.py
+++ utils/screenshot_maker.py
@@ -3,13 +3,13 @@ from datetime import datetime
 
 
 class ScreenshotMaker:
-    def __init__(self, driver, screenshot_dir):
+    def __init__(self, driver, screenshots_dir):
         self._driver = driver
-        self._screenshot_dir = screenshot_dir
+        self._screenshots_dir = screenshots_dir
 
-    def make_screenshot(self, screenshot_name=""):
+    def make_screenshot(self, description=""):
         header_name = "screenshot-" + datetime.now().strftime("date-%y-%m-%d-time-%H-%M-%S-%f")
-        file_name = header_name if not screenshot_name else header_name + f"-{screenshot_name}"
+        file_name = header_name if not description else header_name + "-" + description
         file = file_name + ".png"
-        file_path = os.path.join(self._screenshot_dir, file)
+        file_path = os.path.join(self._screenshots_dir, file)
         self._driver.save_screenshot(file_path)
