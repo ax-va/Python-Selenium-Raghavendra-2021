cfa06fdab7c5f6b0d8c3f24de1a5c3c08e086178
ax-va <vasiliev.informatik@t-online.de>
2023-03-30 17:04:02 +0200

    update

---
+++ examples/example-12-screenshots.py
@@ -0,0 +1,53 @@
+import pathlib
+import sys
+
+from io import BytesIO
+from PIL import Image  # Pillow (installed by pip) that is a friendly fork of PIL, Python Imaging Library
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+# Get the package directory
+package_dir = str(pathlib.Path(__file__).resolve().parents[1])
+# Add the package directory into sys.path if necessary
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
+
+# my modules
+import utils.webdrivers as webdrivers
+from webpages.urls import GOOGLE_URL
+
+
+TIMEOUT = 10  # seconds
+POLLING_EVERY = 0.1  # second
+
+driver = webdrivers.get_chromedriver()
+driver.get(GOOGLE_URL)
+
+try:
+    button_reject_all = driver.find_element(By.ID, "W0wltc")
+    # Save screenshots in PNG.
+    # Two methods are equivalent:
+    driver.save_screenshot("../screenshots/s1_dialog_agreement1.png")
+    driver.get_screenshot_as_file("../screenshots/s1_dialog_agreement2.png")
+    # Save screenshots in multiple formats
+    png_binary_data = driver.get_screenshot_as_png()
+    image = Image.open(BytesIO(png_binary_data))
+    # Save to PNG
+    image.save("../screenshots/s1_dialog_agreement3.png")
+    # Save to JPEG
+    image.convert("RGB").save("../screenshots/s1_dialog_agreement3.jpg")
+except NoSuchElementException:
+    pass
+else:
+    button_reject_all.click()
+
+search_bar = driver.find_element(By.NAME, "q")
+search_bar.send_keys("github ax-va")
+driver.save_screenshot("../screenshots/s2_search_bar_with_text.png")
+search_bar.submit()
+driver.save_screenshot("../screenshots/s3_found_results.png")
+results_item = driver.find_element(By.XPATH, "(//a//*[contains(text(), 'ax-va')])[1]")
+results_item.click()
+driver.save_screenshot("../screenshots/s4_github_page.png")
+
+driver.quit()
