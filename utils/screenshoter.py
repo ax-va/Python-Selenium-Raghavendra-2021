193c1f19037cce1c72c0b41137206706f224a8d9
ax-va <vasiliev.informatik@t-online.de>
2023-03-30 23:21:26 +0200

    update

--- utils/screenshoter.py
+++ utils/screenshoter.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 
 class Screenshoter:
-    def __init__(self, driver, screenshots_dir, head_name="screenshot-"):
+    def __init__(self, driver, screenshots_dir, prefix="screenshot-"):
         self._driver = driver
         self._screenshots_dir = screenshots_dir
-        self._head_name = head_name
+        self._prefix= prefix
         self._counter = 1
 
     @property
@@ -14,8 +14,8 @@ class Screenshoter:
         return self._counter
 
     def take_screenshot(self, description=""):
-        head_name_timestamped = self._head_name + datetime.now().strftime("date-%y-%m-%d-time-%H-%M-%S-%f")
-        file_name = head_name_timestamped if not description else head_name_timestamped + "-" + description
+        prefix_timestamped = self._prefix + datetime.now().strftime("date-%y-%m-%d-time-%H-%M-%S-%f")
+        file_name = prefix_timestamped if not description else prefix_timestamped + "-" + description
         file = file_name + ".png"
         file_path = os.path.join(self._screenshots_dir, file)
         self._driver.save_screenshot(file_path)
