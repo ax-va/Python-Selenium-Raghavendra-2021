d20817ef2037128f59113fbd9544868a89037997
ax-va <vasiliev.informatik@t-online.de>
2023-03-30 14:24:28 +0200

    update

---
+++ tests/test__example_08_4_unittest__exceptions.py
@@ -0,0 +1,55 @@
+"""
+Open directory 'Selenium4-Raghvedra-2021' in terminal and execute the following command:
+------------------------------------------------------------------------
+python -m unittest -v tests.test__example_08_4_unittest__exceptions
+------------------------------------------------------------------------
+
+or discover and execute all the found tests with the command:
+------------------------------
+python -m unittest discover -v
+------------------------------
+"""
+import unittest
+
+
+def raise_value_error_exception(*args, **kwargs):
+    print("This function will throw", ValueError.__name__)
+    print("args: ", args)
+    print("kwargs:", kwargs)
+    raise ValueError("some message")
+
+
+def setUpModule():
+    pass
+
+
+def tearDownModule():
+    pass
+
+
+class TestCase5(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test27(self):
+        """ Check if a method or function throws an exception """
+        args = (1, 2, 3)
+        kwargs = {"a": "A", "b": "B"}
+        self.assertRaises(ValueError, raise_value_error_exception, *args, **kwargs)
+
+# test27 (tests.test__example_08_4_unittest__exceptions.TestCase5)
+# Check if a method or function throws an exception ... This function will throw ValueError
+# args:  (1, 2, 3)
+# kwargs: {'a': 'A', 'b': 'B'}
+# ok
